c54e72a10dbe358facc26af1d77b11cad916c964
Matheus Moura <matheusmoura@ufrn.edu.br>
2020-10-02 10:20:13 -0300

    Criado componente techitem

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     techs: ["Node.js", "ReactJS", "React Native"],
@@ -27,12 +29,7 @@ class TechList extends Component {
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map((tech) => (
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">
-                Remover
-              </button>
-            </li>
+            <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />
           ))}
         </ul>
 
